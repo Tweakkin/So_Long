5ecd966b74f0dd7a8812b31a81ce97b0687d93fc
yd7w <yahyaboukhmira7@gmail.com>
2025-07-03 21:38:41 +0200

    progress

--- keypress_handler.c
+++ keypress_handler.c
@@ -1,13 +1,22 @@
 #include "so_long.h"
 
-int	handle_movements(int keycode, t_game *game)
+int	handle_keypress(int keycode, t_game *game)
 {
 	find_player(game, game->map);
 	if (keycode == 	119 || keycode == 115)
 		move_up_down(keycode, game);
 	if (keycode == 100 || keycode == 97)
 		move_right_left(keycode, game);
+	if (keycode == 65307)
+		handle_closing(game);
 	mlx_clear_window(game->mlx, game->mlx_window);
 	display_map(game);
 	return (0);
+}
+
+int	handle_closing(t_game *game)
+{
+	mlx_destroy_window(game->mlx, game->mlx_window);
+	free_allocated(game);
+	exit(0);
 }
