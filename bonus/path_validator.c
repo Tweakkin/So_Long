c10c0df254ea0f68d73b5fa483e19c002dae0c69
yd7w <yahyaboukhmira7@gmail.com>
2025-07-09 05:28:41 +0200

    done

---
+++ bonus/path_validator.c
@@ -0,0 +1,105 @@
+#include "so_long.h"
+
+int	check_valid_path(t_game *game)
+{
+	char **temp_map;
+
+	temp_map = copy_map(game);
+	if (!temp_map)
+		exit_error(game, "Error: failed to copy map\n");
+	find_player(game, temp_map);
+	floodfill(temp_map, game->x_player, game->y_player);
+	if (is_path_reachable(temp_map, game->lines, game->width) == 0)
+	{
+		free(temp_map);
+		return (0);
+
+	}
+	else
+	{
+		free(temp_map);	
+		return (1);
+	}
+}
+
+char **copy_map(t_game *game)
+{
+	char **dst;
+	int i;
+
+	i = 0;
+	dst = malloc(sizeof(char *) * (game->lines + 1));
+	if (!dst)
+	{
+		free_allocated(game);
+		return NULL;
+	}
+	while (i < game->lines)
+	{
+		dst[i] = malloc(sizeof(char) * (game->width + 1));
+		ft_strcpy(dst[i], game->map[i]);
+		i++;
+	}
+	return (dst);
+}
+
+void	find_player(t_game *game, char **map)
+{
+	int i;
+	int j;
+
+    game->x_player = -1;
+    game->y_player = -1;
+	i = 0;
+	while (i < game->lines)
+	{
+		j = 0;
+		while (j < game->width)
+		{
+			if (map[i][j] == 'P')
+			{
+				game->x_player = j;
+				game->y_player = i;
+				return;
+			}
+			j++;
+		}
+		i++;
+	}
+	exit_error(game, "Error: player not found in map\n");
+}
+
+void	floodfill(char **map, int x, int y)
+{
+	if (y < 0 || x < 0 || map[y] == NULL || map[y][x] == '\0')
+		return;
+	if (map[y][x] == '1' || map[y][x] == 'V')
+		return;
+	if (map[y][x] == 'P' || map[y][x] == '0' || map[y][x] == 'E' || map[y][x] == 'C')
+		map[y][x] = 'V';
+
+	floodfill(map, x + 1, y);
+	floodfill(map, x - 1, y);
+	floodfill(map, x, y + 1);
+	floodfill(map, x, y - 1);
+}
+
+int	is_path_reachable(char **map, int lines, int width)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < lines)
+	{
+		j = 0;
+		while (j < width)
+		{
+			if (map[i][j] == 'E' || map[i][j] == 'C')
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
---
+++ mandatory/render_map.c
@@ -0,0 +1,48 @@
+#include "so_long.h"
+
+void	display_map(t_game *game)
+{
+    int x,;
+	int j;
+    void *img;
+
+    y = 0;
+    while (y < game->lines)
+    {
+        x = 0;
+        while (x < game->width)
+        {
+            if (game->map[y][x] == '0')
+                img = game->img_wall;
+            else if (game->map[y][x] == '1')
+                img = game->img_limits;
+            else if (game->map[y][x] == 'C')
+                img = game->img_collectibles;
+            else if (game->map[y][x] == 'E')
+                img = game->img_exit;
+            else if (game->map[y][x] == 'P')
+                img = game->img_player;
+            else
+                img = game->img_limits;
+            mlx_put_image_to_window(game->mlx, game->mlx_window, img, x * 64, y * 64);
+            x++;
+        }
+        y++;
+    }
+}
+
+void	free_allocated(t_game *game)
+{
+    int i;
+
+    if (!game || !game->map)
+        return;
+    i = 0;
+    while (i < game->lines)
+    {
+        free(game->map[i]);
+        i++;
+    }
+    free(game->map);
+    game->map = NULL;
+}
